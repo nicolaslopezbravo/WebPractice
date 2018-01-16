e4e0a82f3bbfd0291d605dad120122f6b01527c7
Nicolas Lopez <nicolaslopezbravo@gmail.com>
2018-01-15 20:38:17 -0500

    added ajax practices

---
+++ ajax/example3.js
@@ -0,0 +1,9 @@
+function sendMessage(message, callback) {
+    return callback(message);
+}
+
+sendMessage("Message for console", console.log);
+
+sendMessage("Message for alert", alert);
+
+var answer = sendMessage("Are you sure?", confirm);
---
+++ ajax/examples/example5.js
@@ -0,0 +1,10 @@
+function greet(name, formatter) {
+  return "Hello, " + formatter(name);
+}
+
+function upperCaseName(name) {
+  return name.toUpperCase();
+}
+
+greet("Tim", upperCaseName);
+
